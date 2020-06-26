4e94643019d3d11c6283f59ea859c7c88fa223ec
Sherlock-sk <57892533+sherlock-sk@users.noreply.github.com>
2020-06-27 00:32:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 //3000
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port 3000');
+  console.log('app is running on port ${process.env.PORT}');
 })
