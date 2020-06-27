28d4f18cdc71a0389d883f0f98b9d2d36e265a9f
Sherlock-sk <57892533+sherlock-sk@users.noreply.github.com>
2020-06-27 23:59:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'Subodh',
-    password : '',
-    database : 'smart-brain'
+  connection: {connectionString : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
